cdb915f4a9232814b2a41563f9aee5ba12090c23
Ace0x <59582360+Ace0x@users.noreply.github.com>
2025-10-23 22:59:29 +0100

    - Fixed a typo in the BinaryCrossEntropyError class in errors.py. - Refactored DropoutLayer in layers.py for improved clarity and efficiency. - Added a report summarizing findings on overfitting and regularization techniques.

---
+++ B288858/mlp/penalties.py
@@ -0,0 +1,139 @@
+import numpy as np
+
+seed = 22102017
+rng = np.random.RandomState(seed)
+
+
+class L1Penalty(object):
+    """L1 parameter penalty.
+
+    Term to add to the objective function penalising parameters
+    based on their L1 norm.
+    """
+
+    def __init__(self, coefficient):
+        """Create a new L1 penalty object.
+
+        Args:
+            coefficient: Positive constant to scale penalty term by.
+        """
+        assert coefficient > 0., 'Penalty coefficient must be positive.'
+        self.coefficient = coefficient
+
+    def __call__(self, parameter):
+        """Calculate L1 penalty value for a parameter.
+
+        Args:
+            parameter: Array corresponding to a model parameter.
+
+        Returns:
+            Value of penalty term.
+        """
+        return self.coefficient * np.sum(np.abs(parameter))
+        
+
+
+    def grad(self, parameter):
+        """Calculate the penalty gradient with respect to the parameter.
+
+        Args:
+            parameter: Array corresponding to a model parameter.
+
+        Returns:
+            Value of penalty gradient with respect to parameter. This
+            should be an array of the same shape as the parameter.
+        """
+        return self.coefficient * np.sign(parameter)
+        
+
+    def __repr__(self):
+        return 'L1Penalty({0})'.format(self.coefficient)
+
+
+class L2Penalty(object):
+    """L1 parameter penalty.
+
+    Term to add to the objective function penalising parameters
+    based on their L2 norm.
+    """
+
+    def __init__(self, coefficient):
+        """Create a new L2 penalty object.
+
+        Args:
+            coefficient: Positive constant to scale penalty term by.
+        """
+        assert coefficient > 0., 'Penalty coefficient must be positive.'
+        self.coefficient = coefficient
+
+    def __call__(self, parameter):
+        """Calculate L2 penalty value for a parameter.
+
+        Args:
+            parameter: Array corresponding to a model parameter.
+
+        Returns:
+            Value of penalty term.
+        """
+        return self.coefficient * 0.5 * np.sum(parameter ** 2)
+
+    def grad(self, parameter):
+        """Calculate the penalty gradient with respect to the parameter.
+
+        Args:
+            parameter: Array corresponding to a model parameter.
+
+        Returns:
+            Value of penalty gradient with respect to parameter. This
+            should be an array of the same shape as the parameter.
+        """
+        return self.coefficient * parameter
+
+    def __repr__(self):
+        return 'L2Penalty({0})'.format(self.coefficient)
+
+class L1L2MixPenalty(object):
+    """L1 & L2 mix penalty.
+    """
+
+    def __init__(self, l1_coeff, l2_coeff):
+        """Create a new L1 & L2 mix penalty object.
+
+        Args:
+            coefficient: Positive constant to scale penalty term by.
+        """
+         
+        assert l1_coeff >= 0. and l2_coeff >= 0., 'Coefficients must be non-negative.'
+        self.l1_coeff = l1_coeff
+        self.l2_coeff = l2_coeff
+
+
+    
+
+    def __call__(self, parameter):
+        """Calculate L1 & L2 mix penalty value for a parameter.
+
+        Args:
+            parameter: Array corresponding to a model parameter.
+
+        Returns:
+            Value of penalty term.
+        """
+        return (self.l1_coeff * np.sum(np.abs(parameter)) +
+                0.5 * self.l2_coeff * np.sum(parameter ** 2))
+
+    def grad(self, parameter):
+        """Calculate the penalty gradient with respect to the parameter.
+
+        Args:
+            parameter: Array corresponding to a model parameter.
+
+        Returns:
+            Value of penalty gradient with respect to parameter. This
+            should be an array of the same shape as the parameter.
+        """
+        
+        return self.l1_coeff * np.sign(parameter) + self.l2_coeff * parameter
+
+    def __repr__(self):
+           return 'L1L2MixPenalty(l1_coeff={0}, l2_coeff={1})'.format(self.l1_coeff, self.l2_coeff)
